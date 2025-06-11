debcb22ac9a12807a89836acd722d259127c3b04
Alejandro Mora <moraa471@gmail.com>
2025-06-11 15:43:44 -0600

    Add Routing and menu

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { Login } from './Pages/login/login';
+import { NotFound } from './Pages/not-found/not-found';
+import { Home } from './Pages/home/home';
+import { Administration } from './Pages/administration/administration';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'Login',
+        component: Login,
+    },
+    {
+        path: 'Home',
+        component: Home
+    },
+    {
+        path: 'Administration',
+        component: Administration
+
+    },
+    {
+        path: '**',
+        component: NotFound,
+    }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,22 +1,25 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import {  MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterOutlet,RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatListModule,
+    MatIconModule,
+    RouterModule
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected title = 'App Title';
-  protected email = '';
-  protected password = '';
-  protected remember = false;
 
-  onSubmit() {
-    console.log({ email: this.email, password: this.password, remember: this.remember });
-  }
 
 }
